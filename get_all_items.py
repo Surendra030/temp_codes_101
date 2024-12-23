d57f16fd650947b51eed48dba190188a05375e78
Surendra030 <surendrababun030@gmail.com>
2024-12-24 01:49:26 +0530

    None.

---
+++ get_all_items.py
@@ -0,0 +1,90 @@
+import json
+import requests
+from bs4 import BeautifulSoup
+import math
+from decrypt import decrypt_json
+import os
+from mega import Mega
+
+# Example Usage
+passphrase = os.getenv("PASSWORD")  # Replace with your passphrase
+data = decrypt_json("encrypted_data.json", passphrase)
+
+with open("output1.json",'w',encoding='utf-8')as f:
+    json.dump(data,f,indent=4)
+
+
+with open("output1.json",'r',encoding='utf-8')as f:
+    data = json.load(f)
+    data = data[:2]
+
+def upload_tomega():
+    mega  = Mega()
+    keys = os.getenv("M_TOKEN")
+    keys = keys.split("_")
+    m = mega.login(keys[0],keys[1])
+    try:
+        m.upload("data.json")
+    except Exception as e:
+        print("Error Failed to upload : ",e)
+
+
+final_data = []
+try:
+    for index,entry_obj in enumerate(data):
+        obj_href  = entry_obj['href']
+        obj_title = entry_obj['title']
+        
+        obj_total_pages = entry_obj['total_items']
+        if 'k'in obj_total_pages:
+            num = str(obj_total_pages).split('k')[0]
+            obj_total_pages = int(num) * 1000
+        else: obj_total_pages = int(entry_obj['total_items']) 
+
+
+
+
+
+
+        obj_total_pages = math.ceil(obj_total_pages/25)
+        obj_data = []
+        for obj_page in range(obj_total_pages):
+            print(f"{index+1}-{len(data)} => {obj_page}/{obj_total_pages}")
+            temp_link = f"{obj_href}/{obj_page+1}"
+            response = requests.get(temp_link)
+            if response.status_code == 200:
+                soup = BeautifulSoup(response.text, 'html.parser')
+                all_anchors = soup.select(".cover")
+                for anchor in all_anchors:
+                    # Safeguard in case there's no 'img' element inside the anchor
+                    img = anchor.find("img")
+                    img_src = img.get('data-src') if img and img.has_attr('data-src') else str(img)
+
+
+                    
+                    
+                    # Create a dictionary for each anchor element
+                    obj = {
+                        "href": anchor.get('href'),  # Extract 'href' attribute
+                        "img_source": img_src,       # Extract image source URL
+                        "item_title": anchor.get_text(strip=True)  # Extract the title and clean up whitespace
+                    }
+                    
+                    # Add the object to the items list
+                    obj_data.append(obj)
+        final_obj  = {
+            obj_title:obj_data
+        }
+        final_data.append(final_obj)
+
+except Exception as e:
+
+    print("Error : ",e)
+
+finally:
+    with open("data.json",'w',encoding='utf-8')as f:
+        json.dump(final_data,f,indent=4)
+    
+    if os.path.exists("data.json"):
+            upload_tomega()
+    
