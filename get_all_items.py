e85d65921820e1575d19920dbd3b847d235f9b7b
Surendra030 <surendrababun030@gmail.com>
2024-12-30 13:42:06 +0530

    none.

--- get_all_items.py
+++ get_all_items.py
@@ -10,73 +10,30 @@ keys[0] = str(keys[0]).replace('6@','7@')
 mega = Mega()
 m = mega.login(keys[0],keys[1])
 
-try:
-    
-    m.download_url('https://mega.nz/file/1lRBSQIZ#CA2551RiAPjagk_P7UKRfinyc_FLQzP_EFM_fEkUdhM')
-    
-   
-except Exception as e:
-    print('Error failed to download file.')
+handle = 'OdhCgTJR'
 
+all_files = m.get_files()
 
-try:
-    
-    
-    m.download_url('https://mega.nz/file/t1JgzIrD#TruZDnPBu94QLU0PWuIYyBLa1kFLM0DPYreVzwdF04Q')
-except Exception as e:
-    print('Error failed to download file.')
+index = 1
+all_files = []
+for key,snippet in all_files.items():
+    file_name =  snippet['a']['n']
+    if snippet['p'] == handle and '.mp4' in file_name:
+        link = m.export(file_name)
+        obj = {
+            'file_name':file_name,
+            'link':link
+        }
+        all_files.append(obj)
 
-
-print(os.listdir())
-with open('data.json','r',encoding='utf-8')as f:
-    data1 = json.load(f)
-
-with open('data1.json','r',encoding='utf-8')as f:
-    data2 = json.load(f)
-
-
-
-data = [{**d1, **d2} for d1 in data1 for d2 in data2]
-
-
-# Save and compress the JSON file
-compressed_file = 'final_data_json.gz'
-
-with gzip.open(compressed_file, 'wb') as f:
-    json_str = json.dumps(data, indent=4)  # Convert the JSON data to a string
-    f.write(json_str.encode('utf-8'))  # Compress and save the JSON string as binary
-
-print(f"Compressed JSON file saved as: {compressed_file}")
-
-all_ids = set()
-for main_obj in data:
-    for tag_lst in main_obj:
-        all_titles_obj = main_obj[tag_lst]
-        main_tag = tag_lst
-        for one_title_obj in all_titles_obj:
-            if 'href' in one_title_obj:
-                id_part = str(one_title_obj['href']).split('.net/')[-1]
-                all_ids.add(id_part)
-
-
-    
-ids_data = list(all_ids)
-
-with open('ids_lst.json','w',encoding='utf-8')as f:
-    json.dump(ids_data,f,indent=4)
-
-keys[0] = str(keys[0]).replace('6@','7@')
-
-m = mega.login(keys[0],keys[1])
-try:
-    if os.path.exists(compressed_file):
-        print("filesize : ",os.path.getsize(compressed_file))
-        m.upload(compressed_file)
-except Exception as e:
-    print("Failed to upload file.")
+    print(f'{index} / {len(all_files)}')
+    index +=1
 
 try:
-    
-    m.upload('ids_lst.json')
+    with open("links_data.json",'w',encoding='utf-8')as f:
+        json.dump(all_files,f,indent=4)
+    if os.path.exists("links_data.json"):
+        m.upload("links_data.json")
 except Exception as e:
-    print("Failed to upload id-file.")
+    print("Error Failed to upload.",e)
+
