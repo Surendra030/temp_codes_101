4f017f24ce5083dd143ccbf2264faeef355d5d3f
Surendra030 <surendrababun030@gmail.com>
2024-12-31 09:03:20 +0530

    noni.

--- get_all_items.py
+++ get_all_items.py
@@ -10,29 +10,33 @@ keys[0] = str(keys[0]).replace('6@','7@')
 mega = Mega()
 m = mega.login(keys[0],keys[1])
 
-handle = 'OdhCgTJR'
 
-all_files1 = m.get_files()
+all_files = m.get_files()
 
 with open("final.json",'r',encoding='utf-8')as f:
     data = json.load(f)
     
-
-
-
-index = 1
-all_files = []
-
 for obj in data:
-    for old,new in obj.items():
+    for key,snippet in all_files.items():
+        if snippet['h'] == obj['h']:
+            file_name = snippet['a']['n']
+            m.rename(file_name,obj['new_title'])
 
-        for key,snippet in all_files1.items():
-            file_name =  snippet['a']['n']
-            if snippet['p'] == handle and file_name == old:
-                m.rename(file_name,new)
-                
 
-            print(f'{index} / {len(data)}')
-            index +=1
+all_files = m.get_files()
+
+lst = []
+for key,snippet in all_files.items():
+    if snippet['p'] == 'OdhCgTJR':
+        file_name = snippet['a']['n']
+        link = m.export(file_name)
+        obj = {
+            'file_name':file_name,
+            'link':link
+        }
+
+# Step 3: Save the updated data to new.json
+with open("links_data.json", 'w', encoding='utf-8') as f:
+    json.dump(lst, f, indent=4)
 
 
