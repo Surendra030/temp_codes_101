15cefa380d6e7559b8678d4c958826ef6b80c14d
Surendra030 <surendrababun030@gmail.com>
2024-12-31 07:47:52 +0530

    Update get_all_items.py

--- get_all_items.py
+++ get_all_items.py
@@ -26,36 +26,13 @@ all_files = []
 for obj in data:
     for old,new in obj.items():
 
-        for key,snippet in all_files.items():
+        for key,snippet in all_files1.items():
             file_name =  snippet['a']['n']
             if snippet['p'] == handle and file_name == old:
-                m.request_id(file_name,new)
+                m.rename(file_name,new)
                 
 
             print(f'{index} / {len(data)}')
             index +=1
 
 
-
-index = 1
-
-for key,snippet in all_files1.items():
-    file_name =  snippet['a']['n']
-    if snippet['p'] == handle and '.mp4' in file_name:
-        link = m.export(file_name)
-        obj = {
-            'file_name':file_name,
-            'link':link
-        }
-        all_files.append(obj)
-
-    print(f'{index} / {len(all_files)}')
-    index +=1
-
-try:
-    with open("links_data.json",'w',encoding='utf-8')as f:
-        json.dump(all_files,f,indent=4)
-    if os.path.exists("links_data.json"):
-        m.upload("links_data.json")
-except Exception as e:
-    print("Error Failed to upload.",e)
