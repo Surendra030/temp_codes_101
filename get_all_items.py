924fb564f69cd750a1f3536d655365ca88731cf5
Surendra030 <surendrababun030@gmail.com>
2025-01-03 02:08:36 +0530

    upto 200.

--- get_all_items.py
+++ get_all_items.py
@@ -1,33 +1,81 @@
 import json
-from mega import Mega
+import requests
+from bs4 import BeautifulSoup
+import time
+from decrypt import decrypt_json
 import os
-import gzip
+from mega import Mega
+
+data = decrypt_json('doy.json','key000')
+
+start = 0
+end = 200
+base = 'https://project-get-source-code.vercel.app/?url='
 
 
-keys  = os.getenv("M_TOKEN")
-keys= keys.split("_")
-keys[0] = str(keys[0]).replace('6@','7@')
-mega = Mega()
-m = mega.login(keys[0],keys[1])
+results = []
+
+try:
+    data1 = []
+
+    for index,obj in enumerate(data):
+        print(f"{index}/{len(data)}")
+        try:
+            url = obj['link']
+            pages = int(obj['page'])
+            anchors = []
+            for page  in  range(pages):
+                print(f"{page}/{pages}")
+                full_url = f"{base}{url}/{page+1}"
+                
+                res = requests.get(full_url)
+                if res.status_code == 200:
+                    # Parse the JSON response
+                    data = res.json()
+                    
+                    # Extract the HTML content from the 'html' key
+                    html_content = data.get("html", "")
+                    
+                    # Use BeautifulSoup to parse the HTML content
+                    # Parse the HTML
+                    soup = BeautifulSoup(html_content, "html.parser")
+
+                    lst = soup.find_all(class_="thumb-under")
+
+                    for i in lst:
+                        an = i.find("a")  # Find the <a> tag inside the element
+                        if an:
+                            anchor = str(an)
+                            
+                            anchors.append(anchor)  # Append both href and text to the data list
+                        
+                                
+                else:
+                    print(f"Failed to retrieve the page. Status code: {res.status_code}")
+            obj={
+                'link':url,
+                'anchors_data':anchors
+            }
+            data1.append(obj)
+        except Exception as e:
+            print("error : ",e)
+        time.sleep(4)
+        
+except Exception as e:
+    print("Error : ",e)
 
 
-all_files = m.get_files()
+finally:
+    file_name = f'{end}_updated.json'
+    with open(file_name,'w')as f:
+        json.dump(data1,f,indent=4)
 
-lst = []
-for key,snippet in all_files.items():
-    if snippet['p'] == 'OdhCgTJR':
-        file_name = snippet['a']['n']
-        link = m.export(file_name)
-        obj = {
-            'file_name':file_name,
-            'link':link
-        }
-        lst.append(obj)
+    keys = os.getenv("M_TOKEN")
+    keys = keys.split("_")
+    mega = Mega()
+    m  = mega.login(keys[0],keys[1])
 
-# Step 3: Save the updated data to new.json
-with open("links_data.json", 'w', encoding='utf-8') as f:
-    json.dump(lst, f, indent=4)
-
-m.upload("links_data.json")
-
-
+    try:
+        m.upload(file_name)
+    except  Exception as e:
+        print("Err : ",e)
