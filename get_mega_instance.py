4d3da07b3229eadcf6e07b3552e799367c94dd5c
Surendra030 <surendrababun030@gmail.com>
2025-01-27 09:14:46 +0530

    noni

--- get_mega_instance.py
+++ get_mega_instance.py
@@ -12,7 +12,7 @@ def fetch_m():
     keys = os.getenv("M_TOKEN")
     keys = keys.split("_")
     mega  = Mega()
-    keys[0] = keys[0].replace('7@','7@')
+    keys[0] = keys[0].replace('7@','6@')
     m = mega.login(keys[0],keys[1])
     return m
 
